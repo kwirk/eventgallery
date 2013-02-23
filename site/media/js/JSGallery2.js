5cbf0ada5af6c0cfd734fb7a226752f100ccf2f0
svenbluege <>
2013-02-23 22:40:19 +0000

    removed console output

--- site/media/js/JSGallery2.js
+++ site/media/js/JSGallery2.js
@@ -526,12 +526,10 @@ var JSGallery2 = new Class({
 			// fix height of the page-container
 			var maxHeight = 0;
 			$(this.pageContainer).getChildren().each(function(page){
-
 				var height = page.getSize().y;
 				if (height>maxHeight) {
 					maxHeight = height;
 				}
-				console.log(height);
 			});
 
 			this.pageContainer.setStyle('height',maxHeight);
