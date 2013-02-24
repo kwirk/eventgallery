dd7dae97ac5206ba7993ed41624fcf57b30391fb
svenbluege <>
2013-02-24 00:21:56 +0000

    added redirect

--- site/controllers/Checkout.php
+++ site/controllers/Checkout.php
@@ -86,8 +86,8 @@ class CheckoutController extends JControllerLegacy
 		    $this->setRedirect(JRoute::_("index.php?view=checkout"),$msg);
 		}  else {
 			$msg = JText::_('COM_EVENTGALLERY_CART_CHECKOUT_ORDER_STORED');
-			//$app->setUserState( "$option.cart", "" );
-			//$this->setRedirect(JRoute::_("index.php?"),$msg);
+			$app->setUserState( "$option.cart", "" );
+			$this->setRedirect(JRoute::_("index.php?"),$msg);
 		}
 
 		
