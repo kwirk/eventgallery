7c8e6e8142d1f625a077cec3350ae000dd2c4bb4
svenbluege <>
2013-04-15 10:13:15 +0000

    removed DIV in A tags.

--- site/views/events/tmpl/default.php
+++ site/views/events/tmpl/default.php
@@ -30,14 +30,14 @@ $cache = & JFactory::getCache();
 				<?php if (0!=$this->params->get('max_events',9999) && $count>=$this->params->get('max_events',9999)) break; ?>
 				<?php IF ($count < $this->params->get('max_big_events',9999)): ?>
 					<div class="item-container item-container-big">
-						<a class="item item_first" href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>">
+						<div class="item item_first">
 							<div class="content">				
 								<div class="data">
-									<?php IF($this->params->get('show_date',1)==1):?><div class="date"><?php echo JHTML::Date($this->entry->date);?></div><?php ENDIF ?>
-									<div class="title"><?php echo $this->entry->description;?></div>
-									<?php IF($this->params->get('show_text',1)==1):?><div class="text"><?php echo $this->entry->text;?></div><?php ENDIF ?>
-									<?php IF($this->params->get('show_imagecount',1)==1):?><div class="imagecount"><?php echo JText::_('COM_EVENTGALLERY_EVENTS_LABEL_IMAGECOUNT') ?> <?php echo $this->entry->overallCount;?></div><?php ENDIF ?>				
-									<?php IF ($this->params->get('use_comments')==1 && isset($this->entry->commentCount) && $this->params->get('show_commentcount',1)==1):?><div class="comment"><?php echo JText::_('COM_EVENTGALLERY_EVENTS_LABEL_COMMENTCOUNT') ?> <?php echo $this->entry->commentCount;?></div><?php ENDIF ?>
+									<?php IF($this->params->get('show_date',1)==1):?><div class="date"><a href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>"><?php echo JHTML::Date($this->entry->date);?></a></div><?php ENDIF ?>
+									<div class="title"><a  href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>"><?php echo $this->entry->description;?></a></div>
+									<?php IF($this->params->get('show_text',1)==1):?><div class="text"><a  href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>"><?php echo $this->entry->text;?></a></div><?php ENDIF ?>
+									<?php IF($this->params->get('show_imagecount',1)==1):?><div class="imagecount"><a href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>"><?php echo JText::_('COM_EVENTGALLERY_EVENTS_LABEL_IMAGECOUNT') ?> <?php echo $this->entry->overallCount;?></a></div><?php ENDIF ?>				
+									<?php IF ($this->params->get('use_comments')==1 && isset($this->entry->commentCount) && $this->params->get('show_commentcount',1)==1):?><div class="comment"><a href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>"><?php echo JText::_('COM_EVENTGALLERY_EVENTS_LABEL_COMMENTCOUNT') ?> <?php echo $this->entry->commentCount;?></a></div><?php ENDIF ?>
 								</div>
 								
 								<div class="images">
@@ -52,32 +52,34 @@ $cache = & JFactory::getCache();
 										?>
 										
 										<?php $first = true;  foreach($files as $file):?>
-											<?php 
-												if ($first) {
-													echo $file->getThumbImgTag($this->params->get('max_big_events_thumbnails_width',306), $this->params->get('max_big_events_thumbnails_width',306),"large"); 
-													$first = false;
-												} else {
-													
-													$size = ceil($this->params->get('max_big_events_thumbnails_width',306)/sqrt((count($files)-1)));
-													echo $file->getThumbImgTag($size,$size,"small"); 
-													
-												}
-											?>												
+											<a  href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>">
+												<?php 
+													if ($first) {
+														echo $file->getThumbImgTag($this->params->get('max_big_events_thumbnails_width',306), $this->params->get('max_big_events_thumbnails_width',306),"large"); 
+														$first = false;
+													} else {
+														
+														$size = ceil($this->params->get('max_big_events_thumbnails_width',306)/sqrt((count($files)-1)));
+														echo $file->getThumbImgTag($size,$size,"small"); 
+														
+													}
+												?>	
+											</a>											
 										<?php ENDFOREACH?>
 									<?php ENDIF ?>
 									<div style="clear:both"></div>
 								</div>
 							</div>						
-						</a>
+						</div>
 					</div>
 						
 					
 				<?php ELSE:?>
 					<div class="item-container item-container-middle">    
-						<a class="item" href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>">
+						<div class="item">
 							<div class="content">				
 								<?php IF($this->params->get('show_date')==0):?><div class="date"><?php echo JHTML::Date($this->entry->date)?></div><?php ENDIF ?>
-								<div class="title"><?php echo $this->entry->description;?></div>
+								<div class="title"><a href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>"><?php echo $this->entry->description;?></a></div>
 								
 								<?php IF ($this->params->get('show_thumbnails',true) && $count>=$this->params->get('max_big_events',9999) && $count <$this->params->get('max_big_events',9999)+$this->params->get('max_middle_events',0)): ?>
 									<div class="thumbnails">
@@ -88,15 +90,17 @@ $cache = & JFactory::getCache();
 											}
 										?>
 										<?php foreach($files as $file):?>
-										<?php	echo $file->getThumbImgTag($this->params->get('max_middle_events_thumbnails_width',150), $this->params->get('max_middle_events_thumbnails_width',150)); ?>		
+											<a href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>">
+												<?php	echo $file->getThumbImgTag($this->params->get('max_middle_events_thumbnails_width',150), $this->params->get('max_middle_events_thumbnails_width',150)); ?>		
+											</a>
 										<?php ENDFOREACH?>
 									</div>
 								<?php ENDIF?>
-								<?php IF($this->params->get('show_imagecount',1)==1):?><div class="imagecount"><?php echo JText::_('COM_EVENTGALLERY_EVENTS_LABEL_IMAGECOUNT') ?> <?php echo $this->entry->overallCount;?></div><?php ENDIF ?>				
-								<?php IF ($this->params->get('use_comments')==1 && isset($this->entry->commentCount) && $this->params->get('show_commentcount',1)==1):?><div class="comment"><?php echo JText::_('COM_EVENTGALLERY_EVENTS_LABEL_COMMENTCOUNT') ?> <?php echo $this->entry->commentCount;?></div><?php ENDIF ?>
+								<?php IF($this->params->get('show_imagecount',1)==1):?><div class="imagecount"><a href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>"><?php echo JText::_('COM_EVENTGALLERY_EVENTS_LABEL_IMAGECOUNT') ?> <?php echo $this->entry->overallCount;?></a></div><?php ENDIF ?>				
+								<?php IF ($this->params->get('use_comments')==1 && isset($this->entry->commentCount) && $this->params->get('show_commentcount',1)==1):?><div class="comment"><a href="<?php echo JRoute::_("index.php?view=event&folder=".$this->entry->folder) ?>"><?php echo JText::_('COM_EVENTGALLERY_EVENTS_LABEL_COMMENTCOUNT') ?> <?php echo $this->entry->commentCount;?></a></div><?php ENDIF ?>
 							</div>
 							<div class="contentfooter"></div>
-						</a>
+						</div>
 					</div>
 				<?php ENDIF?>
 					    
