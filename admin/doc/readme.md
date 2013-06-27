23e03c3ebfa44451e8bc0a6dfd0db78e7c436954
svenbluege <>
2013-06-27 07:35:53 +0000

    Merged revision(s) 286-289 from branches/event_gallery_2.6.4: removed unnecessary language definitions ........ - docu update ........ added configuration option to use index.php instead of image.php for delivering images. ........

--- admin/doc/readme.md
+++ admin/doc/readme.md
@@ -312,6 +312,11 @@ There are only a few things globally manageable.
 
 	You can define a matrix which is used to sharpen the generated images. By default this is [[-1,-1,-1],[-1,16,-1],[-1,-1,-1]] and it will do a good job. In case you want to have blurry or even sharper images this is the configuration parameter you'll have to touch. Please find the possible values here: http://php.net/manual/en/function.imageconvolution.php . In case you have no clue what this is about, don't touch it. After a change delete the cache otherwise you will see no result.
 
+- Use Rendering Fallback
+
+	On some servers the execution of script is not allowed in subfolders. The execution is necessary since images are delivered by a script and we don't want to run the whole joomla framework for every request. If you encounter issues with images that do not appear, you can use the fallback to use index.php for image delivering. This is `slow and should be not your long term solution`. Contact your provider to make sure php scripts can be executed in component folders. At least this would be great for the following scripts: components/com_eventgallery/helpers/image.php and blank.php.
+
+
 ![Configuration options](img/backend/configuration.jpg)
 
 # Extend {#Extend}
@@ -340,6 +345,7 @@ A simple css definition would look like this. It will give you 4 item per row.
 ## Thumbs do not show up {#FAQThumbs}
 
 ### Server
+- `mod_secure`: Please `ask your provider` about possible issues with that. Doing this solved 99% of the issue.
 - Error 500: PHP memory size too low. I have good experience with 128M. Depends on the size of your images
 - do not use special characters in your file names like Umlaute, +, or things like this. Make your file names safe for the web.
 - Error 500: the PHP function imageconvolution does not work on your server. For now one user reported this using PHP 5.3.8. After commenting out the line of code the gallery worked fine.
@@ -349,6 +355,7 @@ A simple css definition would look like this. It will give you 4 item per row.
 - PHP should have write permission to /images, /cache and /logs
 - Picasa Images do not show up because the method get\_file\_content is not working. Check with your hosting provider to solve this issue.
 - Error 500: PHP should be able to execute the script /components/com_eventgallery/helpers/image.php in order to display thumbs.
+- try to use Use Rendering Fallback and contact your provider if this works for you so he can change the server settings.
 
 ### Joomla Configuration
 - Picasa albums do not work because your SEO-component strips out the @-sign from the URLs
@@ -366,22 +373,29 @@ A simple css definition would look like this. It will give you 4 item per row.
 - your template loads old libraries like MooTools 1.2.5
 - you have a fancy plugin installed which does not care about Joomla an pulls in jQuery without calling jQuery.noConlict() afterwards [jQuery noConlict Documentation](http://api.jquery.com/jQuery.noConflict/)
 - conflict between original LazyLoad and the custom one which is shipped with event gallery (fixed with 2.6.3)
-- conflict between original adavanced MediaBox and the custom one (fixed with 2.6.4)
+- conflict between original advanced MediaBox and the custom one (fixed with 2.6.4)
 
 
 ## Lightbox does not work {#moreFAQ}
 
 - instead of opening an image in a lightbox it opens in a new windows: check for JavaScript errors on your page. Maybe you Joomla Template filters out MooTools, does not load jQuery in no conflict mode or is doing even worse stuff. 
 
+
 ## Images are not properly aligned
 
 - the CSS of your Joomla Template might influence the images 
 - your images are too small to spread to the necessary width
-- the events page shows a dark bar which is larger than the image: your thumbs are too small. Configure the thumb size for the menu item.
+
 
 ## Folders get lost with the sync job
 
 - make sure your files&folder do not contain a dot in the name. This is not supported. Even if I would, this would cause issue with enabled URL rewriting.
+- folder should contain at least one file
+
+
+## Image-Upload does not work
+
+- 100% of the time the upload is refused by the server because of restrictive security configurations. Please contact your provider. They can fix it. In the meantime you can use FTP to upload the images to `/images/eventgallery/[your folder]` and hit the "Sync Database" button in the main tool bar of the Event Gallery component in the back end.
 
 
 # Event Gallery Release Notes {#ReleaseNotes}
@@ -431,6 +445,8 @@ A simple css definition would look like this. It will give you 4 item per row.
 		- added option to display the lightbox in full screen mode. This is very handy if 
 		  you want to support mobile devices since this mode does not waste as much space 
 		  as the current lightbox.
+		- workaround for image delivery problems on some servers. See Use Rendering 
+		  Fallback in the components global configuration options for details.
 
 
 	- Bug fixes
--- site/helpers/image/local.php
+++ site/helpers/image/local.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 /**
  * @package     Sven.Bluege
@@ -8,106 +8,102 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+defined('_JEXEC') or die;	
 
 
-class EventgalleryHelpersImageLocal extends EventgalleryHelpersImageDefault
-{
+class EventgalleryHelpersImageLocal extends EventgalleryHelpersImageDefault{
+
+		protected $_image_script_path = 'components/com_eventgallery/helpers/image.php';
+		protected $_blank_script_path = 'components/com_eventgallery/helpers/blank.php';
+
+		// constructor
+	    public function __construct($photo) {		    
+	    	foreach((array)$photo as $key=>$value) {
+	    		if (substr($key,1,1)!='*') {
+	    			$this->$key = $value;
+	    		}
+	    	}
+
+	    	$app	 = JFactory::getApplication();	   		
+			$params	 = $app->getParams();
+
+			if ($params->get('use_legacy_image_rendering','0')=='1') {
+				$this->_image_script_path = "index.php";
+				$this->_blank_script_path = "components/com_eventgallery/media/images/blank.gif";
+			}
 
 
-    // constructor
-
-    public function __construct($photo)
-    {
-        foreach ((array)$photo as $key => $value) {
-            if (substr($key, 1, 1) != '*') {
-                $this->$key = $value;
-            }
-        }
-    }
-
-    public function getFullImgTag($width = 104, $height = 104)
-    {
-
-        return '<img src="' . JURI::base() . 'components/com_eventgallery/helpers/blank.php?width=' . $width
-        . '&amp;height=' . $height . '"
+	    }
+	    
+	    public function getFullImgTag($width=104,  $height=104) {
+	    	
+	    	return '<img src="'.JURI::base().$this->_blank_script_path.'?width='.$width.'&amp;height='.$height.'" 
 	    	             style="background-repeat:no-repeat;
 	    	    				background-position: 50% 50%; 
-	    	    				background-image:url(\'' . $this->getThumbUrl($width, $height, false, true) . '\');
+	    	    				background-image:url(\''.$this->getThumbUrl($width,$height,false,true).'\');
 	    	    				" 
 	    				alt="" />';
+	    	
+	    }
+	    
+	    public function getThumbImgTag($width=104,  $height=104, $cssClass="", $crop=false) {
+	    	return '<img src="'.JURI::base().$this->_blank_script_path.'?width='.$width.'&amp;height='.$height.'" 
+	    				style="	background-repeat:no-repeat; 
+	    						background-position: 50% 50%; 
+	    						background-image:url(\''.$this->getThumbUrl($width,$height, true, $height==$width).'\');
+								filter: progid:DXImageTransform.Microsoft.AlphaImageLoader( src=\''.$this->getThumbUrl($width,$height, true, $height==$width).'\', sizingMethod=\'scale\'); 
+								-ms-filter: &quot;progid:DXImageTransform.Microsoft.AlphaImageLoader( src=\''.$this->getThumbUrl($width,$height, true, $height==$width).'\', sizingMethod=\'scale\')&quot;;
 
-    }
-
-    public function getThumbUrl($width = 104, $height = 104, $larger = true, $crop = false)
-    {
-
-        if ($crop) {
-            $mode = 'crop';
-        } else {
-            $mode = 'uncrop';
-        }
-
-        if ($height > $width) {
-            $width = $height;
-        }
-
-        return JURI::base() . "components/com_eventgallery/helpers/image.php?option=com_eventgallery&mode=" . $mode
-        . "&width=" . $width . "&view=resizeimage&folder=" . $this->folder . "&file=" . urlencode($this->file);
-    }
-
-    public function getThumbImgTag($width = 104, $height = 104, $cssClass = "", $crop = false)
-    {
-        return '<img src="' . JURI::base() . 'components/com_eventgallery/helpers/blank.php?width=' . $width
-        . '&amp;height=' . $height . '"
-	    				style="	background-repeat:no-repeat;
-	    						background-position: 50% 50%;
-	    						background-image:url(\'' . $this->getThumbUrl($width, $height, true, $height == $width)
-        . '\');
-								filter: progid:DXImageTransform.Microsoft.AlphaImageLoader( src=\''
-        . $this->getThumbUrl($width, $height, true, $height == $width) . '\', sizingMethod=\'scale\');
-								-ms-filter: &quot;progid:DXImageTransform.Microsoft.AlphaImageLoader( src=\''
-        . $this->getThumbUrl($width, $height, true, $height == $width) . '\', sizingMethod=\'scale\')&quot;;
-
-
-	    						"
-	    				alt=""
-	    				class="' . $cssClass . '"/>';
-    }
-
-    public function getLazyThumbImgTag($width = 104, $height = 104, $cssClass = "", $crop = false)
-    {
-        $imgTag = '<img class="lazyme ' . $cssClass . '"
-    									data-width="' . $this->width . '"
-										data-height="' . $this->height . '"
-								    	longdesc="' . $this->getThumbUrl($width, $height, true, $crop) . '"
-								    	src="' . JURI::base() . 'components/com_eventgallery/helpers/blank.php?width='
-            . $width . '&amp;height=' . $height . '"
+								
+	    						" 
+	    				alt="" 
+	    				class="'.$cssClass.'"/>';
+	    }
+	    
+	    public function getLazyThumbImgTag($width=104,  $height=104, $cssClass="", $crop=false) {
+    		$imgTag = '<img class="lazyme '.$cssClass.'"
+    									data-width="'.$this->width.'"
+										data-height="'.$this->height.'"
+								    	longdesc="'.$this->getThumbUrl($width,$height, true, $crop).'"
+								    	src="'.JURI::base().$this->_blank_script_path.'?width='.$width.'&amp;height='.$height.'"
 								    	style="background-position: 50% 50%; background-repeat: no-repeat;"
 								    	alt=""
 					    			/>';
-        return $imgTag;
-    }
+			return $imgTag;
+	    }
+	    
+	    public function getImageUrl($width=104,  $height=104, $fullsize, $larger=false) {
+	    	if ($fullsize) {		    		
+	    		return JURI::base().$this->_image_script_path."?option=com_eventgallery&mode=full&view=resizeimage&folder=".$this->folder."&file=".urlencode($this->file);
+	    	} else {   		
 
-    public function getImageUrl($width = 104, $height = 104, $fullsize, $larger = false)
-    {
-        if ($fullsize) {
-            return JURI::base()
-            . "components/com_eventgallery/helpers/image.php?option=com_eventgallery&mode=full&view=resizeimage&folder="
-            . $this->folder . "&file=" . urlencode($this->file);
-        } else {
+	    		if ($height>$width) {
+	    			$width = $height;
+	    		} 
 
-            if ($height > $width) {
-                $width = $height;
-            }
+		    	return JURI::base().$this->_image_script_path."?option=com_eventgallery&width=".$width."&view=resizeimage&folder=".$this->folder."&file=".urlencode($this->file);
+	    	}
+	    }
+	    
+	    
+	    
+	    public function getThumbUrl ($width=104, $height=104, $larger=true, $crop=false) {	    
+	    	
+	    	if ($crop) {
+	    		$mode = 'crop';
+	    	} else {
+	    		$mode = 'uncrop';
+	    	}	    	    	
 
-            return
-                JURI::base() . "components/com_eventgallery/helpers/image.php?option=com_eventgallery&width=" . $width
-                . "&view=resizeimage&folder=" . $this->folder . "&file=" . urlencode($this->file);
-        }
-    }
-
+	    	if ($height>$width) {
+	    		$width = $height;
+	    	}
+	    				    	
+	    	return JURI::base().$this->_image_script_path."?option=com_eventgallery&mode=".$mode."&width=".$width."&view=resizeimage&folder=".$this->folder."&file=".urlencode($this->file);
+	    }
+	
 }
 
 	
 	
+?>
