e252c16addb1b207791eca80f6c4bdf6adfa0b67
svenbluege <>
2013-06-27 18:48:08 +0000

    fixed sql script

--- admin/sql/install/install.mysql.sql
+++ admin/sql/install/install.mysql.sql
@@ -1,3 +1,19 @@
+CREATE TABLE IF NOT EXISTS `#__eventgallery_comment` (
+  `id` int(11) NOT NULL auto_increment,
+  `file` varchar(125) NOT NULL,
+  `folder` varchar(125) NOT NULL,
+  `text` text NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `published` tinyint(4) NOT NULL default '1',
+  `date` datetime NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `filefolderkey` (`folder`,`file`)
+);
+
 CREATE TABLE IF NOT EXISTS `#__eventgallery_file` (
   `id` int(11) NOT NULL auto_increment,
   `folder` varchar(125) NOT NULL,
@@ -39,13 +55,3 @@ CREATE TABLE IF NOT EXISTS `#__eventgallery_folder` (
   UNIQUE KEY `folder` (`folder`)
 );
 
-
-CREATE TABLE IF NOT EXISTS `#__eventgallery_token` (
-  `id` int(11) NOT NULL auto_increment,
-  `date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `token` varchar(255) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `folder` varchar(125) NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `token` (`token`)
-);
