72199cbb746966f9e3e5554ff98841c827f94e13
Sven Bluege <svenbluege@gmail.com>
2013-07-18 08:34:12 +0200

    - refeactored plugin structure

---
+++ site/plugins/payment/standard/standard.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @package     Sven.Bluege
+ * @subpackage  com_eventgallery
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Sven Bluege All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+class EventgalleryPluginsPaymentStandardStandard extends  EventgalleryLibraryMethodsPayment
+{
+
+
+    /**
+     * Returns if this method can be used with the current cart.
+     *
+     * @param EventgalleryLibraryLineitemcontainer $cart
+     *
+     * @return bool
+     */
+    public function isEligible($cart)
+    {
+       return true;
+    }
+
+    static public  function getClassName() {
+        return "Payment: Standard";
+    }
+}
