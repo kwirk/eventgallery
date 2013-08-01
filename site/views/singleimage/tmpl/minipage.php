8c8ce3ad876be3b0a0e72775e09a0f81a3189205
Sven Bluege <svenbluege@gmail.com>
2013-08-01 13:00:34 +0200

    # removed some useless declarations of the image + added support for sharing images hosted by Google

--- site/views/singleimage/tmpl/minipage.php
+++ site/views/singleimage/tmpl/minipage.php
@@ -27,18 +27,24 @@ if (strlen($this->model->file->title)>0) {
 	$title .= $this->model->file->file;
 }
 
+$imageurl = JURI::base().'images/eventgallery/'.$this->model->file->folder.'/'.$this->model->file->file;
+// handle picasa images
+if (strpos($this->model->file->folder,'@')>0) {
+	$imageurl = $this->model->file->getImageUrl(600, 600, true);
+}
+
 ?><html>
 	<head prefix="og: http://ogp.me/ns#">
 		
-		<meta property="og:image" content="<?php echo  JURI::base().'images/eventgallery/'.$this->model->file->folder.'/'.$this->model->file->file ?>"/>
+		<meta property="og:image" content="<?php echo  $imageurl; ?>"/>
 		<meta property="og:url" content="<?php echo JRoute::_('index.php?option=com_eventgallery&view=singleimage&format=raw&folder='.$this->model->file->folder.'&file='.$this->model->file->file, null, -1)?>"/>
 		<meta property="og:title" content="<?php echo $title ?>"/>
-		<link rel="image_src" type="image/jpeg" href="<?php echo  JURI::base().'images/eventgallery/'.$this->model->file->folder.'/'.$this->model->file->file ?>"/>
+		<link rel="image_src" type="image/jpeg" href="<?php echo $imageurl; ?>"/>
 		<title><?php echo $title ?></title>
 	</head>
 	<body>
 		<a href="<?php echo JRoute::_('index.php?option=com_eventgallery&view=event&folder='.$this->model->file->folder)?>">
-		<img rel="image_src" src="<?php echo  $this->model->file->getImageUrl(600, 600, false) ?>">
+		<img src="<?php echo  $this->model->file->getImageUrl(600, 600, false) ?>">
 		</a>
 	</body>
 </html>
