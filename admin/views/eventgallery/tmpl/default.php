f0b0766d7ef084aa2f0b3beca14a3861d54631cd
Sven Bluege <svenbluege@gmail.com>
2013-08-16 10:42:48 +0200

    updated the overview page with the order statuses

--- admin/views/eventgallery/tmpl/default.php
+++ admin/views/eventgallery/tmpl/default.php
@@ -44,12 +44,54 @@ if ($version->isCompatible('3.0')) {
 			<a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_eventgallery&view=orders')?>"><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_ORDERS')?></a>
 		</div>
 	</div>
-
 	<div class="row-fluid eventgallery-row">
+		<div class="span8 well">
+			<p>
+				If you want to support the development of this component you might want to have a look at my PayPal donation page or at Flattr.
+			</p>
+
+			<div style="margin: 20px 0;">
+				<div style="float: left; margin-top: 10px;">
+					<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=svenbluege%40gmail%2ecom&lc=GB&item_name=Event%20Gallery%20Development&no_note=0&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest"><img src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif"></a>
+				</div>
+				
+				<div style="float: left; margin-left: 40px;">
+					<script type="text/javascript">
+					/* <![CDATA[ */
+					    (function() {
+					        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+					        s.type = 'text/javascript';
+					        s.async = true;
+					        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+					        t.parentNode.insertBefore(s, t);
+					    })();
+					/* ]]> */</script>
+					<a class="FlattrButton" style="display:none;" href="http://www.svenbluege.de/joomla-event-gallery"></a>
+				</div>
+				<div style="clear:both;"></div>
+			</div>
+			<p>
+				For getting support or the lastest version of this component just visit my site: <a href="http://www.svenbluege.de">www.svenbluege.de</a>. 			
+			</p>
+			<p>
+				If you like to file a <strong>feature wish or a defect</strong> please jump over to the tracker: <a href="https://github.com/svenbluege/eventgallery/issues">Event Gallery Tracker</a>
+			</p>
+
+		</div>
+
+
 		<div class="span4">
 			<h2><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_DOCUMENTATION')?></h2>
 			<p><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_DOCUMENTATION_DESC')?></p>
-			<a class="btn btn-default" href="<?php echo JRoute::_('index.php?option=com_eventgallery&view=documentation')?>"><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_DOCUMENTATION')?></a>
+			<a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_eventgallery&view=documentation')?>"><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_DOCUMENTATION')?></a>
+		</div>
+	<div>
+	<div style="clear:both;"></div>
+	<div class="row-fluid eventgallery-row">
+		<div class="span4">
+			<h2><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_ORDERSTATUSES')?></h2>
+			<p><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_ORDERSTATUSES_DESC')?></p>
+			<a class="btn btn-default" href="<?php echo JRoute::_('index.php?option=com_eventgallery&view=orderstatuses')?>"><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_ORDERSTATUSES')?></a>
 		</div>
 		<div class="span4">
 			<h2><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_IMAGETYPES')?></h2>
@@ -64,17 +106,18 @@ if ($version->isCompatible('3.0')) {
 	</div>
 
 	<div class="row-fluid eventgallery-row">
+
 		<div class="span4">
 			<h2><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_SURCHARGES')?></h2>
 			<p><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_SURCHARGES_DESC')?></p>
 			<a class="btn btn-default" href="<?php echo JRoute::_('index.php?option=com_eventgallery&view=surcharges')?>"><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_SURCHARGES')?></a>
 		</div>
-			<div class="span4">
+		<div class="span4">
 			<h2><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_SHIPPINGMETHODS')?></h2>
 			<p><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_SHIPPINGMETHODS_DESC')?></p>
 			<a class="btn btn-default" href="<?php echo JRoute::_('index.php?option=com_eventgallery&view=shippingmethods')?>"><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_SHIPPINGMETHODS')?></a>
 		</div>
-			<div class="span4">
+		<div class="span4">
 			<h2><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_PAYMENTMETHODS')?></h2>
 			<p><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_PAYMENTMETHODS_DESC')?></p>
 			<a class="btn btn-default" href="<?php echo JRoute::_('index.php?option=com_eventgallery&view=paymentmethods')?>"><?php echo JText::_('COM_EVENTGALLERY_SUBMENU_PAYMENTMETHODS')?></a>
@@ -83,32 +126,7 @@ if ($version->isCompatible('3.0')) {
 
 
 
-	<div class="well">
-		<p>
-			If you want to support the development of this component you might want to have a look at my PayPal donation page or at Flattr.
-		</p>
-
-		<p>
-			<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=svenbluege%40gmail%2ecom&lc=GB&item_name=Event%20Gallery%20Development&no_note=0&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest"><img src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif"></a>
-		
-
-			<script type="text/javascript">
-			/* <![CDATA[ */
-			    (function() {
-			        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-			        s.type = 'text/javascript';
-			        s.async = true;
-			        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
-			        t.parentNode.insertBefore(s, t);
-			    })();
-			/* ]]> */</script>
-			<a class="FlattrButton" style="display:none;" href="http://www.svenbluege.de/joomla-event-gallery"></a>
-		</p>
-		<p>
-			For getting support or the lastest version of this component just visit my site: <a href="http://www.svenbluege.de">www.svenbluege.de</a>.
-		</p>
-
-	</div>
+	
 
 </div>
 
