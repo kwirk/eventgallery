0ce9dbfdb7441f1ab1c85f70c959742db87edf90
Sven Bluege <svenbluege@gmail.com>
2013-08-26 13:40:53 +0200

    added module to show the latest event.

---
+++ modules/mod_eventgallery_latest/mod_eventgallery_latest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package     Sven.Bluege
+ * @subpackage  mod_eventgallery_cart
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Sven Bluege All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+// Include the syndicate functions only once
+
+//load classes
+JLoader::registerPrefix('Eventgallery', JPATH_BASE.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.'com_eventgallery');
+
+JModelLegacy::addIncludePath(JPATH_SITE . '/components/com_eventgallery/models', 'ContentModel');
+
+// Load necessary media files
+//EventgalleryHelpersMedialoader::load();
+
+/**
+ * @var JCacheControllerCallback $cache
+ */
+$cache = JFactory::getCache('mod_eventgallery_latest');
+
+/**
+ * @var EventsModelEvents $eventsModel
+ * */
+$eventsModel = JModelLegacy::getInstance('Events', 'EventsModel', array('ignore_request' => true));
+
+$events = $cache->call(
+    array($eventsModel, 'getEntries'),
+        -1,
+        $params->get('max_images', 5),
+        $params->get('tags', ''),
+        $params->get('sort_events_by', 'ordering')
+);
+
+$position = $params->get('event_history_position', 0);
+
+if (count($events)>$position) {
+    $foldername = $events[$position]->folder;
+    /**
+     * @var EventModelEvent $model
+     * */
+    $model = JModelLegacy::getInstance('Event', 'EventModel', array('ignore_request' => true));
+    $folder = $model->getFolder($foldername);
+
+    if (isset($folder) && $folder->published==1 && EventgalleryHelpersFolderprotection::isAccessAllowed($folder)) {
+        $files = $model->getEntries($foldername, 0, $params->get('max_images'), 1);
+        require JModuleHelper::getLayoutPath('mod_eventgallery_latest', $params->get('layout', 'default'));
+    }
+}
---
+++ modules/mod_eventgallery_latest/tmpl/default.php
@@ -0,0 +1,25 @@
+<?php // no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<style>
+    .mod-eventgallery-event .thumbnail{
+        float: left;
+    }
+
+</style>
+
+<div class="mod-eventgallery-event">
+    <div class="thumbnails">
+        <?php foreach($files as $file):?>
+
+                <a class="thumbnail" href="<?php echo JRoute::_(EventgalleryHelpersRoute::createEventRoute($folder->folder, $folder->foldertags)) ?>"><?php
+                    /**
+                     * @var EventgalleryHelpersImageInterface $file
+                     */
+                    echo $file->getThumbImgTag($params->get('thumb_width'), $params->get('thumb_width'), '', true);
+                ?></a>
+
+        <?php endforeach; ?>
+        <div style="clear:both"></div>
+    </div>
+</div>
